583c0bf826d7b237e936673b8e0f247777022550
Spektrainfiniti <sokol2001-01@mail.ru>
2021-12-13 18:43:58 +0300

    Fix

--- base.py
+++ base.py
@@ -1,39 +1,107 @@
-from typing import Any
+from typing import Any, Union
+from dns_prototype import *
 
 
 class Comp:
     """Computer."""
 
     def __init__(self):
-        self.__iface : NetworkInterface = NetworkInterface()
+        self.__iface = NetworkInterface()
         self.__data : Any = None
-
+        self.__local_db : Any = None
+    
+    @property
     def iface(self) -> Any:
         """Return network interface."""
         return self.__iface
 
     def ping(self, addr : str) -> str:
         """Send ping to address."""
-        return self.iface().ping(addr)
+        return self.iface.ping(addr)
 
-    def send(self, data : Any, addr : str) -> str:
+    def send(self, addr : str, type_msg, msg) -> str:
         """Send Data to address."""
-        return self.iface().send(data, addr)
+        message = self.create_message(addr,type_msg, msg)
+        return self.iface.send(message)
 
-    def get(self, data : Any, src : str) -> str:
+    def get(self):
         """Get information from host."""
-        self.add_to_data(data)
-        return f"Data from {src} has been received."
+        msg = self.iface.get()
+        if msg != "No message":
+            if msg[2] == "text":
+                return f"Data from {msg[1]} has been received."
+            elif msg[2] == "resolve":
+                return self.resolve(msg[-1])
+            elif msg[2] == "resolve_non_rec":
+                name = msg[-1]
+                if self.localDb:
+                    addr = self.localDb.resolve(name)
+                    if addr:
+                        return [addr, "IP"]
+                if self.dns:
+                    return [self.dns, "DNS"]
+                return None
 
-    def add_to_data(self, data : Any):
-        """Add information to data in comp."""
-        self.__data = data
+    @property
+    def ip(self) -> str:
+        return self.iface.addr
 
-    def get_ip(self) -> str:
-        return self.iface().addr
-
-    def print_data(self) -> Any:
+    @property
+    def data(self) -> Any:
         return self.__data
+
+    def create_message(self, dst_addr, type_msg, msg):
+        return [dst_addr, self.ip, type_msg, msg]
+
+    @property
+    def dns(self):
+        return self.iface.dns
+
+    @property
+    def localDb(self) -> Any:
+        return self.__local_db
+
+    def set_dns_db(self, db) -> None:
+        self.__local_db = db
+
+    def resolve(self, name : str) -> Union[None, str]:
+        """Resolve name."""
+        if self.localDb:
+            addr = self.localDb.resolve(name)
+            if addr:
+                return addr
+        addr = self.iface.resolve(name)
+        if addr:
+            if self.localDb:
+                self.localDb.add_record(Record(name, addr))
+            else:
+                db = DnsDb()
+                db.add_record(Record(name, addr))
+                self.set_dns_db(db)
+        return addr
+
+    def resolveNonRecursive(self, name : str) -> Union[None, str]:
+        """Resolve name."""
+        if self.localDb:
+            addr = self.localDb.resolve(name)
+            if addr:
+                return addr
+        if self.dns:
+            addr = self.__iface.resolveNonRecursive(self.dns, name)
+            if addr:
+                if type(addr) == list:
+                    addr = addr[0]
+                if self.localDb:
+                    self.localDb.add_record(Record(name, addr))
+                else:
+                    db = DnsDb()
+                    db.add_record(Record(name, addr))
+                    self.set_dns_db(db)
+            return addr
+        return None
+
+    def ant(self):
+        return self.iface.ant
 
 
 class Network:
@@ -41,11 +109,13 @@ class Network:
 
     def __init__(self):
         self.__hosts : dict[str, Comp]= {}
+        self.message_pool : dict[str, list]= {}
 
     def add_host(self, comp : Comp, addr : str) -> None:
         """Add host to net."""
         self.__hosts[addr] = comp
-        comp.iface().setup(self, addr)
+        comp.iface.setup(self, addr)
+        self.message_pool[addr] = []
 
     def ping(self, src : str, dst : str) -> str:
         """Ping sends ping to host."""
@@ -61,8 +131,22 @@ class Network:
 
         return "Unknown host"
 
+    def add_message_to_pool(self, msg):
+        try:
+            self.message_pool[msg[0]].append(msg)
+        except KeyError:
+            return "Unknown host"
+        if msg[0] in self.__hosts:
+            ans = self.__hosts[msg[0]].get()
+            self.message_pool[msg[0]].pop()
+            return ans
+        return "Message added in pool"
+
     def number_hosts(self) -> int:
         return len(self.__hosts)
+
+    def message_to(self, addr):
+        return len(self.message_pool[addr])
 
 
 class NetworkInterface:
@@ -71,6 +155,8 @@ class NetworkInterface:
     def __init__(self):
         self.net : Network = None
         self.addr : str = None
+        self.__dns : Union[None, str] = None
+        self.ant = []
 
     def setup(self, net : Network , addr : str) -> None:
         """Set net and address to interface."""
@@ -83,8 +169,46 @@ class NetworkInterface:
             return "No network"
         return self.net.ping(self.addr, addr)
 
-    def send(self, data : Any, addr : str):
+    def send(self, msg : list):
         """Send Data to address."""
         if not self.net:
             return "No network"
-        return self.net.send(data, self.addr, addr)
+        return self.net.add_message_to_pool(msg)
+
+    def get(self):
+        if not self.net:
+            return "No network"
+        if self.net.message_to(self.addr) > 0:
+            message = self.net.message_pool[self.addr][-1]
+            return message
+        return "No message"
+
+    @property
+    def dns(self):
+        return self.__dns
+    
+    def set_dns_server(self, addr : str) -> None:
+        """Set DNS server."""
+        self.__dns = addr
+
+    def resolve(self, name : str) -> Union[None, str]:
+        """Resolve name."""
+        if not self.net:
+            return None
+        msg = [self.dns, self.addr, "resolve", name]
+        return self.net.add_message_to_pool(msg)
+
+    def resolveNonRecursive(self, dns : str, name : str):
+        """Resolve name."""
+        if not self.net:
+            return None
+        msg = [dns, self.addr, "resolve_non_rec", name]
+        ans = self.net.add_message_to_pool(msg)
+        self.ant.append(ans)
+        if ans:
+            if ans[1] == "IP":
+                return [ans[0]]
+            if ans[1] == "DNS":
+                addr = self.resolveNonRecursive(ans[0], name)
+                return addr[0]
+        return ans
--- test_dns.py
+++ test_dns.py
@@ -1,6 +1,6 @@
-from dns_prototype import *
 import unittest
-
+from base import *
+from dns_prototype import *
 
 class TestRecord(unittest.TestCase):
     def test_record(self):
@@ -49,7 +49,7 @@ class TestDNS_DB(unittest.TestCase):
         self.assertTrue(raised)
 
 
-class TestDns(unittest.TestCase):
+class TestDNS(unittest.TestCase):
     def test_no_local_dns_db(self):
         comp = Comp()
         ans = comp.resolve("narfu.ru")
@@ -69,185 +69,146 @@ class TestDns(unittest.TestCase):
         comp.set_dns_db(db)
         ans = comp.resolve("narfu.ru")
         self.assertEqual(ans, "192.168.0.1")
+    
+    def test_resolve_in_local_db_and_dns(self):
+        comp = Comp()
+        server = Comp()
+        net = Network()
+
+        local_db = DnsDb()
+        local_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        comp.iface.set_dns_server("192.168.0.2")
+        comp.set_dns_db(local_db)
+        
+
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
+
+        ans = comp.resolve("narfu.ru")
+        self.assertEqual(ans, "10.120.10.120")
+
+    def test_answer_from_non_existant_dns_server_recursive(self):
+        comp = Comp()
+        server = Comp()
+        net = Network()
+
+        server_db = DnsDb()
+        server_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        comp.iface.set_dns_server("192.168.0.3")
+        server.set_dns_db(server_db)
+        
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
+
+        ans = comp.resolve("narfu.ru")
+        self.assertEqual(ans, "Unknown host")
 
     def test_answer_from_dns_server_recursive(self):
         comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "192.168.0.1"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("192.192.192.192")
-
         server = Comp()
-        server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "168.168.168.168"))
-        server.set_dns_db(server_db)
-
         net = Network()
-        net.add_host(comp, "10.10.10.10")
-        net.add_host(server, "192.192.192.192")
 
-        ans = comp.resolve("ya.ru")
-        self.assertEqual(ans, "168.168.168.168")
+        server_db = DnsDb()
+        server_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        comp.iface.set_dns_server("192.168.0.2")
+        server.set_dns_db(server_db)
+        
+
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
+
+        ans = comp.resolve("narfu.ru")
+        self.assertEqual(ans, "10.120.10.120")
 
     def test_answer_from_dns_server_non_recursive(self):
         comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "192.168.0.1"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("192.192.192.192")
-
         server = Comp()
+        net = Network()
+
         server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "168.168.168.168"))
+        server_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        comp.iface.set_dns_server("192.168.0.2")
         server.set_dns_db(server_db)
+        
 
-        net = Network()
-        net.add_host(comp, "10.10.10.10")
-        net.add_host(server, "192.192.192.192")
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
 
-        ans = comp.resolveNonRecursive("ya.ru")
-        self.assertEqual(ans, "168.168.168.168")
+        ans = comp.resolveNonRecursive("narfu.ru")
+        self.assertEqual(ans, "10.120.10.120")
 
-    def test_wrong_addr_of_dns_server(self):
+    def test_answer_from_non_existant_dns_server_non_recursive(self):
         comp = Comp()
-        comp.set_dns_db(DnsDb())
-        comp.iface().set_dns_server("10.20.30.45")
-
-        net = Network()
-        net.add_host(comp, "11.12.13.14")
-
-        ans = comp.resolve("ya.ru")
-        self.assertIsNone(ans)
-
-    def test_resolve_unknown_name_recursive(self):
-        comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "1.2.3.4"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("10.20.30.40")
-
         server = Comp()
-        server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "2.3.4.5"))
-        server.set_dns_db(server_db)
-
         net = Network()
-        net.add_host(comp, "11.12.13.14")
-        net.add_host(server, "10.20.30.40")
 
-        ans = comp.resolve("ya.com")
-        self.assertIsNone(ans)
-
-    def test_resolve_unknown_name_non_recursive(self):
-        comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "1.2.3.4"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("10.20.30.40")
-
-        server = Comp()
         server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "2.3.4.5"))
+        server_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        comp.iface.set_dns_server("192.168.0.3")
         server.set_dns_db(server_db)
+        
 
-        net = Network()
-        net.add_host(comp, "11.12.13.14")
-        net.add_host(server, "10.20.30.40")
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
 
-        ans = comp.resolveNonRecursive("ya.com")
-        self.assertIsNone(ans)
-    
-    def test_main_dns_resolve_recursive(self):
-        comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "1.2.3.4"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("10.20.30.40")
-
-        server = Comp()
-        server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "2.3.4.5"))
-        server.set_dns_db(server_db)
-        server.iface().set_dns_server("20.30.40.50")
-
-        main_server = Comp()
-        main_server_local_db = DnsDb()
-        main_server_local_db.add_record(Record("ya.com", "3.4.5.6"))
-        main_server.set_dns_db(main_server_local_db)
-
-        net = Network()
-        net.add_host(comp, "11.12.13.14")
-        net.add_host(server, "10.20.30.40")
-        net.add_host(main_server, "20.30.40.50")
-
-        ans = comp.resolve("ya.com")
-        self.assertEqual(ans, "3.4.5.6")
-
+        ans = comp.resolveNonRecursive("narfu.ru")
+        self.assertEqual(ans, "Unknown host")
+        
     def test_main_dns_resolve_non_recursive(self):
         comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "1.2.3.4"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("10.20.30.40")
-
         server = Comp()
-        server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "2.3.4.5"))
-        server.set_dns_db(server_db)
-        server.iface().set_dns_server("20.30.40.50")
-
         main_server = Comp()
-        main_server_local_db = DnsDb()
-        main_server_local_db.add_record(Record("ya.com", "3.4.5.6"))
-        main_server.set_dns_db(main_server_local_db)
-
         net = Network()
-        net.add_host(comp, "11.12.13.14")
-        net.add_host(server, "10.20.30.40")
-        net.add_host(main_server, "20.30.40.50")
 
-        ans = comp.resolveNonRecursive("ya.com")
-        self.assertEqual(ans, "3.4.5.6")
-
-    def test_caching_recursive(self):
-        comp = Comp()
         local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "192.168.0.1"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("192.192.192.192")
-
-        server = Comp()
         server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "168.168.168.168"))
+        main_server_db = DnsDb()
+
+        local_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        server_db.add_record(Record("ya.ru", "10.10.10.10"))
+        main_server_db.add_record(Record("29.ru", "100.100.100.100"))
+
+        comp.set_dns_db(local_db)
         server.set_dns_db(server_db)
+        main_server.set_dns_db(main_server_db)
 
-        net = Network()
-        net.add_host(comp, "10.10.10.10")
-        net.add_host(server, "192.192.192.192")
+        comp.iface.set_dns_server("192.168.0.2")
+        server.iface.set_dns_server("192.168.0.3")
 
-        comp.resolve("ya.ru")
-        ans = comp.localDb().num_records()
-        self.assertEqual(ans, 2)
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
+        net.add_host(main_server, "192.168.0.3")
 
-    def test_caching_non_recursive(self):
+        ans = comp.resolveNonRecursive("29.ru")
+        self.assertEqual(ans, "100.100.100.100")
+
+    def test_main_dns_resolve_recursive(self):
         comp = Comp()
-        local_db = DnsDb()
-        local_db.add_record(Record("narfu.ru", "192.168.0.1"))
-        comp.set_dns_db(local_db)
-        comp.iface().set_dns_server("192.192.192.192")
-
         server = Comp()
-        server_db = DnsDb()
-        server_db.add_record(Record("ya.ru", "168.168.168.168"))
-        server.set_dns_db(server_db)
-
+        main_server = Comp()
         net = Network()
-        net.add_host(comp, "10.10.10.10")
-        net.add_host(server, "192.192.192.192")
 
-        comp.resolveNonRecursive("ya.ru")
-        ans = comp.localDb().num_records()
-        self.assertEqual(ans, 2)
+        local_db = DnsDb()
+        server_db = DnsDb()
+        main_server_db = DnsDb()
+
+        local_db.add_record(Record("narfu.ru", "10.120.10.120"))
+        server_db.add_record(Record("ya.ru", "10.10.10.10"))
+        main_server_db.add_record(Record("29.ru", "100.100.100.100"))
+
+        comp.set_dns_db(local_db)
+        server.set_dns_db(server_db)
+        main_server.set_dns_db(main_server_db)
+
+        comp.iface.set_dns_server("192.168.0.2")
+        server.iface.set_dns_server("192.168.0.3")
+
+        net.add_host(comp, "192.168.0.1")
+        net.add_host(server, "192.168.0.2")
+        net.add_host(main_server, "192.168.0.3")
+
+        ans = comp.resolve("29.ru")
+        self.assertEqual(ans, "100.100.100.100")
 
 
 if __name__ == '__main__':
